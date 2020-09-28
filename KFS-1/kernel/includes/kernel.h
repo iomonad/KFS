27c74c119593d929bb023f430addfe1d39f1e4fa
Clement T <me@trosa.io>
2020-09-28 16:46:37 +0200

    Kernel WIP

---
+++ KFS-1/kernel/includes/kernel.h
@@ -0,0 +1,15 @@
+/*
+ * (c) iomonad - <iomonad@riseup.net>
+ */
+#ifndef KERNEL_H
+#define KERNEL_H
+
+#include "types.h"
+
+/* SYSTEM CONSTANTS */
+
+#define PAGE_SIZE 4096
+
+#define NULL (void*)0
+
+#endif	/* KERNEL_H */
---
+++ KFS-1/kernel/includes/types.h
@@ -0,0 +1,13 @@
+/*
+ * (c) iomonad - <iomonad@riseup.net>
+ */
+#ifndef TYPES_H
+#define TYPES_H
+
+/* Concrete types */
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+
+#endif	/* TYPES_H */
