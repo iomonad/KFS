e48a6442d3ee01c36e6ea77e815d044e0995db2d
Clement T <me@trosa.io>
2020-10-05 11:24:20 +0200

    Update

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,7 +2,7 @@
 
 KERNEL_TARGET=kfs.rom
 QEMU_PLATFORM=qemu-system-x86_64
-QEMU_OPTS=-curses -serial stdio -vga std -m 128
+QEMU_OPTS=-curses -m 128 -s
 
 if [ ! -f "${KERNEL_TARGET}" ]; then
     make -C $(pwd) rom
