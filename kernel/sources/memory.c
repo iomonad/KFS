1aa309b8e8d9929e3227935fe2dd67047358210c
Clement T <me@trosa.io>
2020-10-07 16:59:30 +0200

    Bitset algorithm work in progress

--- kernel/sources/memory.c
+++ kernel/sources/memory.c
@@ -7,14 +7,20 @@
  *
  */
 #include <vga.h>
+#include <irq.h>
 #include <memory.h>
 #include <kernel.h>
+#include <lifecycle.h>
 
 /* Reference from linker file */
 extern uint32_t physmemstart;
 
 /* Memory referenced from linker value */
 kheap_t served_address = (uint32_t)&physmemstart;
+
+/* Cursor & Kernel directory pages */
+page_directory_t *curr_dir_page = NULL;
+page_directory_t *kernel_dir_page = NULL;
 
 /*
  * Kernel memory implementation management, with allignement
@@ -56,9 +62,34 @@ uint32_t kpmalloc(const uint32_t size, uint32_t *phyref)
 	return __kmalloc_impl(size, 1, phyref);
 }
 
-/*  */
+/* Bitset algorithms implementation */
+
+
+static void initialize_pagging(page_directory_t *dir)
+{
+	uint32_t cr0_reg;
+
+	curr_dir_page = dir;
+	asm volatile("mov %0, %%cr3":: "r"(&dir->physical_table));
+	asm volatile("mov %%cr0, %0": "=r"(cr0_reg));
+	cr0_reg |= 0x80000000; 	/* This enable paging according x86 */
+	asm volatile("mov %0, %%cr0":: "r"(cr0_reg));
+}
+
+/* Page fault interrupt callback */
+static void page_fault_int(registers_t r)
+{
+	(void)r;
+	/* TODO: Need debug address print */
+	kpanic("Page Fault");
+}
+
 void __attribute__ ((cold))
 install_system_memory(void)
 {
-	return ;
+	/* Physical memory Size */
+	//uint32_t memory_end_page = 0x1000000;
+
+	register_interrupt_callback(14, page_fault_int);
+	return initialize_pagging(kernel_dir_page); /*  */
 }
